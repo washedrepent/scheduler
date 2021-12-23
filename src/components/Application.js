1535e55112705319f9e5042dfaa28584334334ea
Hans-Eric Lippke <hans_eric_lippke@hotmail.com>
2021-12-23 14:49:06 -0500

    Added day state to application

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import DayList from "./DayList";
 
 import "components/Application.scss";
 export default function Application(props) {
+    const [day, setDay] = useState("Monday");
+
     const days = [
         {
             id: 1,
@@ -30,11 +32,7 @@ export default function Application(props) {
                 />
                 <hr className='sidebar__separator sidebar--centered' />
                 <nav className='sidebar__menu'>
-                    <DayList
-                        days={days}
-                        day={"Monday"}
-                        setDay={(day) => console.log(day)}
-                    />
+                    <DayList days={days} day={day} setDay={setDay} />
                 </nav>
                 <img
                     className='sidebar__lhl sidebar--centered'
