009c71544320439ff39b7f941529c3e7fc6238d4
Hans-Eric Lippke <hans_eric_lippke@hotmail.com>
2022-01-04 12:19:30 -0500

    Added Appointment component and story

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayList from "components/DayList";
 import DayListItem from "components/DayListItem";
 import InterviewerList from "components/InterviewerList";
 import InterviewerListItem from "components/InterviewerListItem";
+import Appointment from "components/Appointment";
 
 storiesOf("Button", module)
     .addParameters({
@@ -131,3 +132,9 @@ storiesOf("InterviewerList", module)
             onChange={(event) => action("setInterviewer")(interviewer.id)}
         />
     ));
+storiesOf("Appointment", module)
+    .addParameters({
+        backgrounds: [{ name: "white", value: "#fff", default: true }],
+    })
+    .add("Appointment", () => <Appointment />)
+    .add("Appointment with Time", () => <Appointment time='12pm' />);
