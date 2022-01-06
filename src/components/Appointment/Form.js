f0b3227c0f1d048d4fb016c5ba91511cf2006b25
Hans-Eric Lippke <hans_eric_lippke@hotmail.com>
2022-01-06 12:25:16 -0500

    Added the Form component and Edit/Create stories for appointment

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+
+export default function Form(props) {
+    const [interviewer, setInterviewer] = useState(props.interviewer);
+
+    return (
+        <main className='appointment__card appointment__card--create'>
+            <section className='appointment__card-left'>
+                <form autoComplete='off'>
+                    <input
+                        className='appointment__create-input text--semi-bold'
+                        name='name'
+                        type='text'
+                        placeholder='Enter Student Name'
+                        /*
+          This must be a controlled component
+        */
+                    />
+                </form>
+                <InterviewerList
+                    interviewers={props.interviewers}
+                    value={interviewer}
+                    onChange={setInterviewer}
+                />
+            </section>
+            <section className='appointment__card-right'>
+                <section className='appointment__actions'>
+                    <Button onClick={props.onCancel} danger>
+                        Cancel
+                    </Button>
+                    <Button onClick={props.onSave} confirm>
+                        Save
+                    </Button>
+                </section>
+            </section>
+        </main>
+    );
+}
--- stories/index.js
+++ stories/index.js
@@ -17,8 +17,9 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
-storiesOf("Button", module)
+storiesOf("Butt3on", module)
     .addParameters({
         backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
     })
@@ -167,5 +168,21 @@ storiesOf("Appointment", module)
         <Error
             message='Could not delete appointment.'
             onClose={action("onClose")}
+        />
+    ))
+    .add("Edit", () => (
+        <Form
+            name='Lydia Miller-Jones'
+            interviewers={interviewers}
+            interviewer={3}
+            onSave={action("onSave")}
+            onCancel={action("onCancel")}
+        />
+    ))
+    .add("Create", () => (
+        <Form
+            interviewers={interviewers}
+            onSave={action("onSave")}
+            onCancel={action("onCancel")}
         />
     ));
