17725fa2c75b7f77f9937447cf29160b65ceedf8
Hans-Eric Lippke <hans_eric_lippke@hotmail.com>
2022-01-06 17:36:38 -0500

    Added axios and removed hard coded days object to use api instead

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import DayList from "./DayList";
 
 import "components/Application.scss";
@@ -56,24 +57,20 @@ const appointments = [
 
 export default function Application(props) {
     const [day, setDay] = useState("Monday");
+    const [days, setDays] = useState([]);
 
-    const days = [
-        {
-            id: 1,
-            name: "Monday",
-            spots: 2,
-        },
-        {
-            id: 2,
-            name: "Tuesday",
-            spots: 5,
-        },
-        {
-            id: 3,
-            name: "Wednesday",
-            spots: 0,
-        },
-    ];
+    useEffect(() => {
+        axios
+
+            .get("http://localhost:8001/api/days")
+            .then((response) => {
+                setDays(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
     return (
         <main className='layout'>
             <section className='sidebar'>
