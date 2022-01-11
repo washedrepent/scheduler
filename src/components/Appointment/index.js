3d15333f18e6698129fe0677bf8341bf09289559
Hans-Eric Lippke <hans_eric_lippke@hotmail.com>
2022-01-11 12:57:45 -0500

    Implemented custom hook in the interview to transition from empty to create and back

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,21 +2,37 @@ import React from "react";
 import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
+import Form from "./Form";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+    const { mode, transition, back } = useVisualMode(
+        props.interview ? SHOW : EMPTY
+    );
+
     return (
         <article className='appointment'>
             <Header time={props.time} />
-            {props.interview && props.interview.interviewer ? (
+            {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+            {mode === SHOW && (
                 <Show
                     student={props.interview.student}
                     interviewer={props.interview.interviewer}
-                    onDelete={props.onDelete}
-                    onEdit={props.onEdit}
                 />
-            ) : (
-                <Empty onAdd={props.onAdd} />
+            )}
+            {mode === CREATE && (
+                <Form
+                    interviewers={[]}
+                    onCancel={back}
+                    onSave={(interview) => {
+                        transition(SHOW, interview);
+                    }}
+                />
             )}
         </article>
     );
